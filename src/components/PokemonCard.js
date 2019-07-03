c5516952a3a9272fffad88a22e0dc30c8aa1ca67
github username <gregm9999@gmail.com>
2019-07-02 17:48:19 -0700

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,27 +1,44 @@
-import React from 'react'
-import { Card } from 'semantic-ui-react'
+import React from "react";
+import { Card } from "semantic-ui-react";
 
 class PokemonCard extends React.Component {
+  state = {
+    seeFront: true
+  };
+
+  handleCardSideToggle = () => {
+    this.setState({ seeFront: !this.state.seeFront });
+  };
+
   render() {
+    const hp =
+      this.props.pokemon.stats.find(stat => stat.name === "hp").value || 40;
     return (
-      <Card>
+      <Card onClick={this.handleCardSideToggle}>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img
+              src={
+                this.state.seeFront
+                  ? this.props.pokemon.sprites.front
+                  : this.props.pokemon.sprites.back
+              }
+              alt="oh no!"
+            />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {hp}
             </span>
           </div>
         </div>
       </Card>
-    )
+    );
   }
 }
 
-export default PokemonCard
+export default PokemonCard;
